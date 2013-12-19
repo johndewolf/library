54dbad143e126e93a932bc47bd94dad5137c404c
johndewolf <jdewolf06@gmail.com>
2013-12-19 15:30:25 -0500

    removed comments

--- spec/models/categorization_spec.rb
+++ spec/models/categorization_spec.rb
@@ -7,12 +7,4 @@ describe Categorization do
   it { should validate_presence of :book }
   it { should validate_presence of :category }
 
-  # it "has many books" do
-  #   book1 = Book.create(title: 'foo')
-  #   book2 = Book.create(title: 'bar')
-  #   category1 = Category.create(name: 'faz')
-  #   Categorization.create(book: book1, category: category1)
-  #   Categorization.create(book: book2, category: category1)
-  #   expect(Categorization.all.count).to eq(2)
-  # end
 end
